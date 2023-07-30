5228a197f2a181a361fa267d5cf6e774c3248ddf
rpakishore <rpakishore@gmail.com>
2023-07-31 01:45:55 +0300

    removed stop

--- recreate.sh
+++ recreate.sh
@@ -1,11 +1,11 @@
 #export $(grep -v '^#' EDIT.env | xargs)
-cmd="docker ps -aqf \"name=slabs\""
-id=$(docker ps -aqf "name=slabs")
+#cmd="docker ps -aqf \"name=slabs\""
+#id=$(docker ps -aqf "name=slabs")
 
 git pull
 
 docker build -t slabs:latest .
-docker container stop $id
-docker container rm $id
+#docker container stop $id
+#docker container rm $id
 
 docker compose up -d
